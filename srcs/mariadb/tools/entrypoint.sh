d2df479e370dc64a492e5146a76c300db405bfeb
mustafa <mradwan@student.42abudhabi.ae>
2023-09-12 11:38:32 -0400

    '

--- srcs/mariadb/tools/entrypoint.sh
+++ srcs/mariadb/tools/entrypoint.sh
@@ -3,8 +3,11 @@
 # Start the MariaDB service
 service mariadb start
 
+# mysqld --user=mysql --bootstrap
 # Run the create_user.sh script
 bash /create_user.sh
 
 # Keep the container running (this prevents it from exiting)
-tail -f /dev/null
+# tail -f /dev/null
+exec mysqld --skip-log-error --user=root
+# mysqld --user=root
