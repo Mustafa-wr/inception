67531122009489b857d9a6a60a6285b995a7a4ee
mustafa <mradwan@student.42abudhabi.ae>
2023-09-12 11:52:11 -0400

    '

--- srcs/wordpress/conf/config.sh
+++ srcs/wordpress/conf/config.sh
@@ -1,37 +1,15 @@
-
 #! /bin/bash
 cd /var/www/wordpress
 
-echo hello wordddd
-# if [ ! -f wp-config.php ]; then
-	echo hello world
-    wp core download --force --allow-root
-    # wp config create --dbname=$DB_NAME  --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
-    wp --allow-root --path=/var/www/wordpress core install \
+wp core download --force --allow-root
+wp config create --path=/var/www/wordpress --dbname=$DB_NAME  --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root --dbprefix=wp_
+wp --allow-root --path=/var/www/wordpress core install \
 	--url='mradwan.42.fr' --title='WordPress' \
 	--skip-email --admin_email="$WP_ADMIN_MAIL" \
 	--admin_user="$WP_ADMIN_USER" \
 	--admin_password="$WP_ADMIN_PASSWORD"
-	wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PASSWORD --role=subscriber --porcelain --allow-root
-# fi
+wp user create $WP_USER $WP_USER_MAIL --user_pass=$WP_USER_PASSWORD --role=subscriber --porcelain --allow-root
+
 
 php-fpm7.4 -F
 
-# cd /var/www/wordpress
-# wp core config	--dbhost=$DB_HOST \
-# 				--dbname=$DB_NAME \
-# 				--dbuser=$DB_USER \
-# 				--dbpass=$DB_PASSWORD \
-# 				--allow-root
-
-# wp core install --title=$WP_TITLE \
-# 				--admin_user=$WP_ADMIN_USER \
-# 				--admin_password=$WP_ADMIN_PASSWORD \
-# 				--admin_email=$WP_ADMIN_MAIL \
-# 				--url=$WP_URL \
-# 				--allow-root
-
-# wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
-# cd -
-
-# php-fpm7.4 -F
